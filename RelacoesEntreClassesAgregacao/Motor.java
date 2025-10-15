5cce8f209677f1ca4baab63e5c43b5acd71022f4
Beatriz <becapivaraa@gmail.com>
2025-10-15 14:42:48 -0300

    Adicionando três projetos de relações entre classes

---
+++ RelacoesEntreClassesAgregacao/Motor.java
@@ -0,0 +1,18 @@
+package RelacoesEntreClassesAgregacao;
+
+public class Motor {
+
+	private String tipo;
+
+	public Motor(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+}
---
+++ RelacoesEntreClassesAgregacao/PrincipalCarro.java
@@ -0,0 +1,14 @@
+package RelacoesEntreClassesAgregacao;
+
+public class PrincipalCarro {
+
+	public static void main(String[] args) {
+
+		Motor motor = new Motor("V8");
+		Carro carro = new Carro("Mustang", motor);
+
+		System.out.println("Carro é: " + carro.getModelo() + "/nMotor: " + motor.getTipo());
+
+	}
+
+}
